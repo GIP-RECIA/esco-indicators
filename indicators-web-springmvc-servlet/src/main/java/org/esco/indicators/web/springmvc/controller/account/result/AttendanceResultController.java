cb67fa940754fe1e7aa0c8c5167af218c2018dca
kfrapin <kevin.frapin@gmail.com>
2012-07-10 14:09:47 +0200

    Addition of monitoring attendance result controller

--- indicators-web-springmvc-servlet/src/main/java/org/esco/indicators/web/springmvc/controller/account/result/AttendanceResultController.java
+++ indicators-web-springmvc-servlet/src/main/java/org/esco/indicators/web/springmvc/controller/account/result/AttendanceResultController.java
@@ -10,13 +10,10 @@ import java.util.GregorianCalendar;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.esco.indicators.domain.beans.form.AccountActivationForm;
-import org.esco.indicators.domain.beans.form.BasicForm;
 import org.esco.indicators.domain.beans.result.ResultRow;
-import org.esco.indicators.services.form.DataFormService;
 import org.esco.indicators.services.form.ResultFormService;
 import org.esco.indicators.services.structure.EstablishmentService;
 import org.esco.indicators.utils.constants.web.SessionConstants;
@@ -33,168 +30,29 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.ibm.icu.util.Calendar;
 
 /**
- * Controller handling the requests on the page displaying the results of a form submission.
+ * Controller handling the requests on the page displaying the results.<br/>
+ * The results contained in this page only concern the monitoring type : {@link DataFormConstants#JSP_KEY_ATTENDANCE}.
  * 
  * @since  2012/06/29
  * @author GIP RECIA - Kevin Frapin <kevin.frapin@recia.fr>
  */
 @Controller
 @RequestMapping("/accounts-activations-attendance-result")
-public class AttendanceResultController extends BasicController {
+public class AttendanceResultController extends BasicResultController {
     //---------------------------------------------------------------------------------- ATTRIBUTES
     /** Logger of the class */
     private static final Logger LOGGER = Logger.getLogger(AttendanceResultController.class);
 
-    /** Establishment service providing access to establishments data */
-    @Autowired
-    private EstablishmentService establishmentService;
-    
-    /** Service providing access to result data */
-    @Autowired
-    private ResultFormService resultFormService;
-    
     //-------------------------------------------------------------------------------- CONSTRUCTORS
     /**
      * Default constructor of the {@link AttendanceResultController} class.
      */
     public AttendanceResultController() {
-	super();
+	super("accounts-activations-attendance-result");
     }
 
-    //--------------------------------------------------------------------------- GETTERS / SETTERS
-   /**
-     * Sets the service providing access to establisments data.
-     * @param establishmentService 
-     * 			The ervice providing access to establisments data to set.
-     */
-    public void setEstablishmentService(EstablishmentService establishmentService) {
-        this.establishmentService = establishmentService;
-    }
-    
-     /**
-      * Sets the service providing access to the data form.
-      * 
-     * @param dataFormService 
-     * 			The service providing access to the data form to set.
-     */
-    public void setDataFormService(DataFormService dataFormService) {
-        this.dataFormService = dataFormService;
-    }
-
-    /**
-     * Sets the service providing access to result data.
-     * 
-     * @param resultFormService 
-     * 			The service providing access to result data to set.
-     */
-    public void setResultFormService(ResultFormService resultFormService) {
-        this.resultFormService = resultFormService;
-    }
-    
     //------------------------------------------------------------------------------ PUBLIC METHODS
 
-    /**
-     * Initializes the result view and redirect to the view.
-     * 
-     * @param model
-     * 			Model data.
-     * @param request
-     * 			Request made by the user.
-     * @return
-     * 	the JSP view name.
-     */
-    @RequestMapping(method = RequestMethod.GET)
-    public String initForm(ModelMap model, HttpServletRequest request){
-	// Verification of the presence of a submitted form
-	if(!containsForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR)) {
-	    LOGGER.warn("No submitted form has been found in the user session, so there is no data to process and display. The user is redirected to the welcome page.");
-	    return "redirect:welcome";
-	}
-	
-        // Cleaning of the session
-	request.getSession().setAttribute(SessionConstants.ACCOUNT_FORM_ATTR, null);
-	return "accounts-activations-attendance-result";
-    }
-    
-
-    /**
-     * Populate the county field.
-     * 
-     * @param request
-     * 			The request made by the user.
-     * @return
-     * 	the submitted value for the county field.
-     */
-    @ModelAttribute("countyItem")
-    public String populateCounty(HttpServletRequest request) {
-	// Checks if the there is a valid submitted form to process
-	if(!containsForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR)) {
-	    return null;
-	}
-	
-	// Retrieval of the submitted monitoring type value
-	AccountActivationForm aaForm = (AccountActivationForm) getSessionForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR);
-	
-	// Retrieval of the i18n key
-	String jspKey = aaForm.getCounty();
-	String i18nKey = null;
-	if(jspKey != null) {
-	    i18nKey = dataFormService.getI18nKey(jspKey);
-	}
-
-	return i18nKey;
-    }
-    
-    /**
-     * Populate the end date field.
-     * 
-     * @param request
-     * 			The request made by the user.
-     * @return
-     * 	the submitted value for the end date field.
-     */
-    @ModelAttribute("endDateItem")
-    public String populateEndDate(HttpServletRequest request) {
-	// Checks if the there is a valid submitted form to process
-	if(!containsForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR)) {
-	    return null;
-	}
-	
-	// Retrieval of the submitted monitoring type value
-	AccountActivationForm aaForm = (AccountActivationForm) getSessionForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR);
-	
-	// Retrieval of the end date
-	String endDate = aaForm.getEndDatePicker();
-	
-	return endDate;
-    }
-    
-    /**
-     * Populate the establishments types field.
-     * 
-     * @param request
-     * 			The request made by the user.
-     * @return
-     * 	the submitted values for the establishments types field.
-     */
-    @ModelAttribute("estbalishmentsTypesItems")
-    public List<String> populateEstablishmentsTypes(HttpServletRequest request) {
-	// Checks if the there is a valid submitted form to process
-	if(!containsForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR)) {
-	    return null;
-	}
-	
-	// Retrieval of the submitted monitoring type value
-	AccountActivationForm aaForm = (AccountActivationForm) getSessionForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR);
-	
-	// Retrieval of the i18n key
-	String [] establishmentsTypes = aaForm.getEstablishmentsTypes();
-	List<String> jspKeys = new ArrayList<String>(Arrays.asList(establishmentsTypes));
-	List<String> i18nKeys = getI18nKeys(jspKeys);
-	
-	return i18nKeys;
-    }
-    
     /**
      * Populate the field containing the list of the filtered users profiles.
      * 
@@ -205,102 +63,19 @@ public class AttendanceResultController extends BasicController {
      */
     @ModelAttribute("filteredUsersProfilesItems")
     public List<String> populateFilteredUsersProfiles(HttpServletRequest request) {
-	// Checks if the there is a valid submitted form to process
-	if(!containsForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR)) {
-	    return null;
-	}
-	
-	// Retrieval of the submitted monitoring type value
-	AccountActivationForm aaForm = (AccountActivationForm) getSessionForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR);
-	
-	// Retrieval of the users profiles to filter
-	List<String> checkedProfiles = new ArrayList<String>(Arrays.asList(aaForm.getUsersProfiles()));
-	List<String> usersProfilesToFilter = dataFormService.getUsersProfilesToFilter(checkedProfiles);
-	
-	return usersProfilesToFilter;
-    }
-    
-    /**
-     * Populate the 'lycees' types field.
-     * 
-     * @param request
-     * 			The request made by the user.
-     * @return
-     * 	the submitted values for the 'lycees' types field.
-     */
-    @ModelAttribute("lyceesTypesItems")
-    public List<String> populateLyceesTypes(HttpServletRequest request) {
-	// Checks if the there is a valid submitted form to process
-	if(!containsForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR)) {
-	    return null;
-	}
-	
-	// Retrieval of the submitted monitoring type value
-	AccountActivationForm aaForm = (AccountActivationForm) getSessionForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR);
-	
-	// Retrieval of the i18n key
-	String [] lyceesTypes = aaForm.getLyceesTypes();
-	List<String> i18nKeys = null;
-	if(lyceesTypes != null) {
-		List<String> jspKeys = new ArrayList<String>(Arrays.asList(lyceesTypes));
-		i18nKeys = getI18nKeys(jspKeys);
-	}
-	
-	return i18nKeys;
-    }
-    
-    /**
-     * Populate the 'la' types field.
-     * 
-     * @param request
-     * 			The request made by the user.
-     * @return
-     * 	the submitted values for the 'la' types field.
-     */
-    @ModelAttribute("laTypesItems")
-    public List<String> populateLaTypes(HttpServletRequest request) {
-	// Checks if the there is a valid submitted form to process
-	if(!containsForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR)) {
-	    return null;
-	}
-	
-	// Retrieval of the submitted monitoring type value
-	AccountActivationForm aaForm = (AccountActivationForm) getSessionForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR);
-	
-	// Retrieval of the i18n key
-	String [] lyceesTypes = aaForm.getLaTypes();
-	List<String> i18nKeys = null;
-	if(lyceesTypes != null) {
-		List<String> jspKeys = new ArrayList<String>(Arrays.asList(lyceesTypes));
-		i18nKeys = getI18nKeys(jspKeys);
-	}
-	
-	return i18nKeys;
-    }
-    
-    /**
-     * Populate the monitoring  type field.
-     * 
-     * @param request
-     * 			The request made by the user.
-     * @return
-     * 	the submitted value for the monitoring  type  field.
-     */
-    @ModelAttribute("monitoringTypeItem")
-    public String populateMonitoringType(HttpServletRequest request) {
-	// Checks if the there is a valid submitted form to process
-	if(!containsForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR)) {
-	    return null;
-	}
-	
-	// Retrieval of the submitted monitoring type value
-	AccountActivationForm aaForm = (AccountActivationForm) getSessionForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR);
-	
-	// Retrieval of the i18n key
-	String jspKey = aaForm.getMonitoringType();
-	String i18nKey = dataFormService.getI18nKey(jspKey);
-	
-	return i18nKey;
+        // Checks if the there is a valid submitted form to process
+        if(!containsForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR)) {
+            return null;
+        }
+        
+        // Retrieval of the submitted monitoring type value
+        AccountActivationForm aaForm = (AccountActivationForm) getSessionForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR);
+        
+        // Retrieval of the users profiles to filter
+        List<String> checkedProfiles = new ArrayList<String>(Arrays.asList(aaForm.getUsersProfiles()));
+        List<String> usersProfilesToFilter = dataFormService.getUsersProfilesToFilter(checkedProfiles);
+        
+        return usersProfilesToFilter;
     }
     
     /**
@@ -335,147 +110,14 @@ public class AttendanceResultController extends BasicController {
 	Date startDate = aaForm.getStartDate();
 	
 	// Gets the result rows to display
-	List<ResultRow> resultRows = getResultRows(establishmentsTypes, establishmentsUai, usersProfilesToFilter, startDate);
+	List<ResultRow> resultRows = createResultRows(establishmentsTypes, establishmentsUai, usersProfilesToFilter, startDate);
 	
 	return resultRows;
     }
     
-    /**
-     * Populate the start date field.
-     * 
-     * @param request
-     * 			The request made by the user.
-     * @return
-     * 	the submitted value for the start date field.
-     */
-    @ModelAttribute("startDateItem")
-    public String populateStartDate(HttpServletRequest request) {
-	// Checks if the there is a valid submitted form to process
-	if(!containsForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR)) {
-	    return null;
-	}
-	
-	// Retrieval of the submitted monitoring type value
-	AccountActivationForm aaForm = (AccountActivationForm) getSessionForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR);
-	
-	// Retrieval of the start date
-	String startDate = aaForm.getStartDatePicker();
-	
-	return startDate;
-    }
     
-    /**
-     * Populate the sum on counties field.
-     * 
-     * @param request
-     * 			The request made by the user.
-     * @return
-     * 	the submitted value for the sum on counties field.
-     */
-    @ModelAttribute("sumOnCountiesItem")
-    public String populateSumOnCounties(HttpServletRequest request) {
-	// Checks if the there is a valid submitted form to process
-	if(!containsForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR)) {
-	    return null;
-	}
-	
-	// Retrieval of the submitted monitoring type value
-	AccountActivationForm aaForm = (AccountActivationForm) getSessionForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR);
-	
-	// Retrieval of the i18n key
-	String jspKey = aaForm.getSumOnCounties();
-	String i18nKey = null;
-	if(jspKey != null) {
-	    i18nKey = dataFormService.getI18nKey(jspKey);
-	}
-	
-	return i18nKey;
-    }
-    
-    /**
-     * Populate the users profiles field.
-     * 
-     * @param request
-     * 			The request made by the user.
-     * @return
-     * 	the submitted values for the users profiles field.
-     */
-    @ModelAttribute("usersProfilesItems")
-    public List<String> populateUsersProfiles(HttpServletRequest request) {
-	// Checks if the there is a valid submitted form to process
-	if(!containsForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR)) {
-	    return null;
-	}
-	
-	// Retrieval of the submitted monitoring type value
-	AccountActivationForm aaForm = (AccountActivationForm) getSessionForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR);
-	
-	// Retrieval of the i18n key
-	String [] usersProfiles = aaForm.getUsersProfiles();
-	List<String> jspKeys = new ArrayList<String>(Arrays.asList(usersProfiles));
-	List<String> i18nKeys = getI18nKeys(jspKeys);
-	
-	return i18nKeys;
-    }
 
     //----------------------------------------------------------------------------- PRIVATE METHODS
-    
-    /**
-     * Checks if the specified session contains a form associated to the session attribute <code>formAttribute</code>.<br/>
-     * 
-     * @param session
-     * 			The user session.
-     * @param formAttribute
-     * 			The session attribute associated to the form.
-     * 
-     * @return
-     * 	<code>true</code> if the session contained a form associated to <code>formAttribute</code>.<br/>
-     * 	<code>false</code> in other cases.
-     */
-    private boolean containsForm(HttpSession session, String formAttribute) {
-	// Retrieval of the associated form
-	BasicForm basicForm = (BasicForm) session.getAttribute(formAttribute);
-	if(basicForm == null) {
-	    return false;
-	}
-	return true;
-    }
-    
-    /**
-     * Gets the form associated to the session attribute : <code>formAttribute</code>.
-     * 
-     * @param session
-     * 			The user session.
-     * @param formAttribute
-     * 			The session attribute associated to the form.
-     * 
-     * @return
-     * 	the form associated to the session attribute : <code>formAttribute</code>.<br/>.
-     * 	<code>null</code> if no form is associated to the session attribute.
-     */
-    private BasicForm getSessionForm(HttpSession session, String formAttribute) {
-	// Retrieval of the form
-	return (BasicForm) session.getAttribute(formAttribute);
-    }
-    
-    /**
-     * Gets the i18n keys associated to the specified JSP keys.
-     * 
-     * @param jspKeys
-     * 			The JSP keys associated to the i18n keys to retrieve.
-     * 
-     * @return
-     * 	the list of the i18n keys associated to the JSP keys.<br/>
-     * 	an empty list if no i18n keys has been retrieved.
-     */
-    private List<String> getI18nKeys(List<String> jspKeys) {
-	// Retrieval of the i18n keys
-	List<String> i18nKeys = new ArrayList<String>();
-	for (String jspKey : jspKeys) {
-	    i18nKeys.add(dataFormService.getI18nKey(jspKey));
-	}
-	return i18nKeys;
-    }
     
     /**
      * Creates the result row; each result row containing the following data :
@@ -496,10 +138,7 @@ public class AttendanceResultController extends BasicController {
      * @return
      * 	the result rows containing the data to display.
      */
-    private List<ResultRow> getResultRows( List<String> establishmentsTypes, List<String> establishmentsUai,List<String> usersProfiles, Date startDate) {
-	// Final result
-	List<ResultRow> rows =  new ArrayList<ResultRow>();
-	
+    private List<ResultRow> createResultRows( List<String> establishmentsTypes, List<String> establishmentsUai,List<String> usersProfiles, Date startDate) {
 	// Retrieval of the year
 	Integer year = DateUtils.getYear(startDate);
 	
@@ -509,13 +148,11 @@ public class AttendanceResultController extends BasicController {
 	) {
 	    // If the only selected establishment type is : CFA
 	    Integer week = DateUtils.getWeekOfYear(startDate);
-	    rows.addAll(resultFormService.getPunctualWeekResultRows(establishmentsUai, usersProfiles, week, year));
-	} else {
-	    Integer month = DateUtils.getMonthOfYear(startDate);
-	    rows.addAll(resultFormService.getPunctualMonthResultRows(establishmentsUai, usersProfiles, month, year));
-	}
+	    return resultFormService.getPunctualWeekResultRows(establishmentsUai, usersProfiles, week, year);
+	} 
 	
-	return rows;
+	Integer month = DateUtils.getMonthOfYear(startDate);
+	return resultFormService.getPunctualMonthResultRows(establishmentsUai, usersProfiles, month, year);
     }
     
     //------------------------------------------------------------------------------ STATIC METHODS
--- indicators-web-springmvc-servlet/src/main/java/org/esco/indicators/web/springmvc/controller/account/result/MonitoringAttendanceResultController.java
+++ indicators-web-springmvc-servlet/src/main/java/org/esco/indicators/web/springmvc/controller/account/result/MonitoringAttendanceResultController.java
@@ -3,24 +3,230 @@
  */
 package org.esco.indicators.web.springmvc.controller.account.result;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.log4j.Logger;
+import org.esco.indicators.domain.beans.form.AccountActivationForm;
+import org.esco.indicators.domain.beans.result.ResultRow;
+import org.esco.indicators.domain.beans.util.IntegerPair;
+import org.esco.indicators.utils.constants.web.SessionConstants;
+import org.esco.indicators.utils.constants.xml.DataFormConstants;
+import org.esco.indicators.utils.date.DateUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * @since  
+ * Controller handling the requests on the page displaying the results.<br/>
+ * The results contained in this page only concern the monitoring type : {@link DataFormConstants#JSP_KEY_MONITORING_ATTENDANCE}.
+ * 
+ * @since  2012/07/10
  * @author GIP RECIA - Kevin Frapin <kevin.frapin@recia.fr>
  */
-public class MonitoringAttendanceResultController {
+@Controller
+@RequestMapping("/accounts-activations-monitoring-attendance-result")
+public class MonitoringAttendanceResultController extends BasicResultController {
     //---------------------------------------------------------------------------------- ATTRIBUTES
     /** Logger of the class */
     private static final Logger LOGGER = Logger.getLogger(MonitoringAttendanceResultController.class);
 
     //-------------------------------------------------------------------------------- CONSTRUCTORS
+    /**
+     * Constructor of the {@link MonitoringAttendanceResultController} class.
+     * @param viewName
+     */
+    public MonitoringAttendanceResultController() {
+	super("accounts-activations-monitoring-attendance-result");
+    }
 
     //--------------------------------------------------------------------------- GETTERS / SETTERS
 
     //------------------------------------------------------------------------------ PUBLIC METHODS
-
+    
+    
+    /**
+     * Populate the attribute containing all the statistic periods represented by pairs ({@link IntegerPair}).<br/>
+     * Each pair contains :
+     * <ul>
+     * 	<li>First value : number of a week (or month)</li>
+     * 	<li>Second value : year of the first value</li>
+     * </ul>
+     * 
+     * @param request
+     * 			The request made by the user.
+     * 
+     * @return
+     * 	the statistic periods used to index the statistic data in the result rows.
+     */
+    public List<IntegerPair> populateStatisticPeriods(HttpServletRequest request) {
+        // Checks if the there is a valid submitted form to process
+        if(!containsForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR)) {
+            return null;
+        }
+        
+        // Retrieval of the submitted monitoring type value
+        AccountActivationForm aaForm = (AccountActivationForm) getSessionForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR);
+        
+	// Retrieval of the establishments types
+	List<String> establishmentsTypes = new ArrayList<String>(Arrays.asList(aaForm.getEstablishmentsTypes()));
+	
+        // Retrieval of the start and end date
+        Date startDate = aaForm.getStartDate();
+        Date endDate = aaForm.getEndDate();
+        
+        return getStatisticPeriods(establishmentsTypes, startDate, endDate);
+        
+    }
+    
+    /**
+     * Populate the end date field.
+     * 
+     * @param request
+     * 			The request made by the user.
+     * @return
+     * 	the submitted value for the end date field.
+     */
+    @ModelAttribute("endDateItem")
+    public String populateEndDate(HttpServletRequest request) {
+        // Checks if the there is a valid submitted form to process
+        if(!containsForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR)) {
+            return null;
+        }
+        
+        // Retrieval of the submitted monitoring type value
+        AccountActivationForm aaForm = (AccountActivationForm) getSessionForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR);
+        
+        // Retrieval of the end date
+        String endDate = aaForm.getEndDatePicker();
+        
+        return endDate;
+    }
+    
+    /**
+     * Populates the data rows of the table used to display the result of the submitted form.
+     * 
+     * @param request
+     * 			The request made by the user.
+     * @return
+     * 	the data rows of the table used to display the result of the submitted form.
+     */
+    @ModelAttribute("tableRowsItems")
+    public List<ResultRow> populateTableRows(HttpServletRequest request) {
+	// Checks if the there is a valid submitted form to process
+	if(!containsForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR)) {
+	    return null;
+	}
+	
+	// Retrieval of the submitted form
+	AccountActivationForm aaForm = (AccountActivationForm) getSessionForm(request.getSession(), SessionConstants.ACCOUNT_FORM_ATTR);
+	
+	// Retrieval of the establishments uai
+	List<String> establishmentsUai = new ArrayList<String>(Arrays.asList(aaForm.getEstablishments()));
+	
+	// Retrieval of the establishments types
+	List<String> establishmentsTypes = new ArrayList<String>(Arrays.asList(aaForm.getEstablishmentsTypes()));
+	
+	// Retrieval of the users profiles to filter
+	List<String> checkedProfiles = new ArrayList<String>(Arrays.asList(aaForm.getUsersProfiles()));
+	String userProfileToFilter = dataFormService.getUsersProfilesToFilter(checkedProfiles).get(0);
+	
+	// Retrieval of the start date and end date
+	Date startDate = aaForm.getStartDate();
+	Date endDate = aaForm.getEndDate();
+	
+	// Gets the result rows to display
+	List<ResultRow> resultRows = createResultRows(establishmentsTypes, establishmentsUai, userProfileToFilter, startDate, endDate);
+	
+	return resultRows;
+    }
+    
     //----------------------------------------------------------------------------- PRIVATE METHODS
-
+    /**
+     * Creates the result rows; each result row containing the following data :
+     * <ul>
+     * 	<li>The establishment data (name, UAI,..)</li>
+     * 	<li>The statistic data (number of connections,...)</li>
+     * </ul>
+     * 
+     * The statistic data are indexed by {@link IntegerPair} containing :
+     * <ul>
+     * 	<li>First value : the number of a week / month</li>
+     * 	<li>Second value : the year of the week</li>
+     * </ul>
+     * The periods (week/month and year) represented by the pairs are extracted from the original period
+     * specified by the <code>startDate</code> and the <code>endDate</code>.
+     * 
+     * @param establishmentsTypes
+     * 			The types of the establishments.
+     * @param establishmentsUai
+     * 			The UAI of the establishments.
+     * @param userProfile
+     * 			The user profile concerned by the statistics.
+     * @param startDate
+     * 			The start date of the statistics.
+     * @param endDate
+     * 			The end date of the statistics.
+     * 
+     * @return
+     * 	the result rows containing the data to display.
+     */
+    private List<ResultRow> createResultRows( List<String> establishmentsTypes, List<String> establishmentsUai,String userProfile, Date startDate, Date endDate) {
+	// Retrieval of the start and end years
+	Integer startYear = DateUtils.getYear(startDate);
+	Integer endYear = DateUtils.getYear(endDate);
+	
+	// Retrieval of the month / or week
+	if(	establishmentsTypes.contains(DataFormConstants.JSP_KEY_CFA) 
+		&& establishmentsTypes.size() == 1
+	) {
+	    // If the only selected establishment type is : CFA
+	    Integer startWeek = DateUtils.getWeekOfYear(startDate);
+	    Integer endWeek = DateUtils.getWeekOfYear(endDate);
+	    return resultFormService.getWeeklyResultRows(establishmentsUai, userProfile, startWeek, startYear, endWeek, endYear);
+	}
+			
+	Integer startMonth = DateUtils.getMonthOfYear(startDate);
+	Integer endMonth = DateUtils.getMonthOfYear(endDate);
+	return resultFormService.getMonthlyResultRows(establishmentsUai, userProfile, startMonth, startYear, endMonth, endYear);
+    }
+    
+    /**
+     * Gets the statistic periods present in the specified period starting with the <code>startDate</code> and ending with the <code>endDate</code>.<br/>
+     * These periods will be split by weeks if the only selected establishment type is {@link DataFormConstants#JSP_KEY_CFA}.<br/>
+     * Else, the periods will be split by months.
+     * 
+     * @param establishmentsTypes
+     * 			The establishments types selected in the user view.
+     * @param startDate
+     * 			The start
+     * @param endDate
+     * 
+     * @return
+     * 	the statistic periods.
+     */
+    private List<IntegerPair> getStatisticPeriods(List<String> establishmentsTypes, Date startDate, Date endDate) {
+	// Get the start and end years
+	Integer startYear = DateUtils.getYear(startDate);
+	Integer endYear = DateUtils.getYear(endDate);
+	
+	// Retrieval of the periods by month / or by week
+	if(	establishmentsTypes.contains(DataFormConstants.JSP_KEY_CFA) 
+		&& establishmentsTypes.size() == 1
+	) {
+	    // If the only selected establishment type is : CFA
+	    Integer startWeek = DateUtils.getWeekOfYear(startDate);
+	    Integer endWeek = DateUtils.getWeekOfYear(endDate);
+	    return DateUtils.splitWeeks(startWeek, startYear, endWeek, endYear);
+	}
+	   
+	// TODO
+	return null;
+    }
+    
     //------------------------------------------------------------------------------ STATIC METHODS
 }
