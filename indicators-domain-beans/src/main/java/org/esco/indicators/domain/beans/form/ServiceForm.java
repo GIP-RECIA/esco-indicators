d20e1f4bd4ce9de990bdd5f66a74787a04e24518
kfrapin <kevin.frapin@gmail.com>
2012-07-24 14:46:46 +0200

    equals, hashCode and toString

--- indicators-domain-beans/src/main/java/org/esco/indicators/domain/beans/form/ServiceForm.java
+++ indicators-domain-beans/src/main/java/org/esco/indicators/domain/beans/form/ServiceForm.java
@@ -3,6 +3,8 @@
  */
 package org.esco.indicators.domain.beans.form;
 
+import java.util.Arrays;
+
 import org.apache.log4j.Logger;
 
 /**
@@ -65,9 +67,55 @@ public class ServiceForm extends BasicForm {
     public void setWantedServices(String[] services) {
         this.wantedServices = services;
     }
+
     
     //------------------------------------------------------------------------------ PUBLIC METHODS
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = super.hashCode();
+	result = prime * result + Arrays.hashCode(availableServices);
+	result = prime * result + Arrays.hashCode(wantedServices);
+	return result;
+    }
+    
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (!super.equals(obj))
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	ServiceForm other = (ServiceForm) obj;
+	if (!Arrays.equals(availableServices, other.availableServices))
+	    return false;
+	if (!Arrays.equals(wantedServices, other.wantedServices))
+	    return false;
+	return true;
+    }
 
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+	return "ServiceForm [availableServices=" + Arrays.toString(availableServices) + ", wantedServices="
+		+ Arrays.toString(wantedServices) + ", monitoringType=" + monitoringType
+		+ ", establishmentsTypes=" + Arrays.toString(establishmentsTypes) + ", startDatePicker="
+		+ startDatePicker + ", startDate=" + startDate + ", endDatePicker=" + endDatePicker
+		+ ", endDate=" + endDate + ", usersProfiles=" + Arrays.toString(usersProfiles) + ", county="
+		+ county + ", sumOnCounties=" + sumOnCounties + ", lyceesTypes="
+		+ Arrays.toString(lyceesTypes) + ", laTypes=" + Arrays.toString(laTypes)
+		+ ", establishments=" + Arrays.toString(establishments) + "]";
+    }
+    
     //----------------------------------------------------------------------------- PRIVATE METHODS
 
     //------------------------------------------------------------------------------ STATIC METHODS
