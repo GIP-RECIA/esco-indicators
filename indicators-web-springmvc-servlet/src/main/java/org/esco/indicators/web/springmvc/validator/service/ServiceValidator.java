d5b96564ef6812e5d13668bcc5ed103513824653
kfrapin <kevin.frapin@gmail.com>
2012-07-24 16:56:39 +0200

    Creation of the service form validator

---
+++ indicators-web-springmvc-servlet/src/main/java/org/esco/indicators/web/springmvc/validator/service/ServiceValidator.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package org.esco.indicators.web.springmvc.validator.service;
+
+import org.apache.log4j.Logger;
+import org.esco.indicators.domain.beans.form.AccountActivationForm;
+import org.esco.indicators.domain.beans.form.ServiceForm;
+import org.esco.indicators.utils.constants.web.FormValidationConstants;
+import org.esco.indicators.utils.constants.xml.DataFormConstants;
+import org.esco.indicators.web.springmvc.validator.basic.BasicValidator;
+import org.springframework.validation.Errors;
+
+/**
+ * Validator of the {@link ServiceForm}.
+ * 
+ * @since  2012/07/24
+ * @author GIP RECIA - Kevin Frapin <kevin.frapin@recia.fr>
+ */
+public class ServiceValidator extends BasicValidator {
+    //---------------------------------------------------------------------------------- ATTRIBUTES
+    /** Logger of the class */
+    private static final Logger LOGGER = Logger.getLogger(ServiceValidator.class);
+
+    //-------------------------------------------------------------------------------- CONSTRUCTORS
+
+    //--------------------------------------------------------------------------- GETTERS / SETTERS
+
+    //------------------------------------------------------------------------------ PUBLIC METHODS
+    /* (non-Javadoc)
+     * @see org.springframework.validation.Validator#supports(java.lang.Class)
+     */
+    @Override
+    public boolean supports(Class<?> clazz) {
+	return ServiceForm.class.equals(clazz);
+    }
+
+    /* (non-Javadoc)
+     * @see org.springframework.validation.Validator#validate(java.lang.Object, org.springframework.validation.Errors)
+     */
+    @Override
+    public void validate(Object target, Errors errors) {
+	/////////////////////////////////////////////////////////////
+	// Validation of the basic fields
+	/////////////////////////////////////////////////////////////
+	super.validate(target, errors);
+	
+	/////////////////////////////////////////////////////////////
+	// Retrieval of the form
+	/////////////////////////////////////////////////////////////
+	ServiceForm aaForm = (ServiceForm) target;
+	
+	/////////////////////////////////////////////////////////////
+	// Validation of the number of selected users
+	// profiles
+	/////////////////////////////////////////////////////////////
+	String [] usersProfiles = (String []) errors.getFieldValue(DataFormConstants.USERS_PROFILES);
+	Integer maxNumProfiles = FormValidationConstants.MAX_SELECTED_PROFILES_SERVICE_FORM;
+	if(usersProfiles != null && usersProfiles.length > maxNumProfiles) {
+	    // If there are more selected profiles than the maximum allowed
+	    errors.rejectValue(DataFormConstants.USERS_PROFILES, "error.form.usersProfiles.maxValue", new Object [ ] { maxNumProfiles }, "error.form.usersProfiles.maxValue");
+	}
+	
+	/////////////////////////////////////////////////////////////
+	// Validation of the selected services
+	/////////////////////////////////////////////////////////////
+	// TODO
+    }
+    
+    //----------------------------------------------------------------------------- PRIVATE METHODS
+
+    //------------------------------------------------------------------------------ STATIC METHODS
+}
