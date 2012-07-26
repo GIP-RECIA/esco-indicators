e14bd0f36a7014b9975f973a0d05204b6e8325e1
kfrapin <kevin.frapin@gmail.com>
2012-07-26 16:25:36 +0200

    Result services for the service form

---
+++ indicators-domain-services/src/main/java/org/esco/indicators/services/form/service/ResultServiceFormService.java
@@ -0,0 +1,81 @@
+/**
+ * 
+ */
+package org.esco.indicators.services.form.service;
+
+import java.util.List;
+
+import org.esco.indicators.domain.beans.result.ResultRow;
+
+/**
+ * Interface providing functions for retrieving the result data to put into the result web page
+ * concernaning the services statistics.
+ * 
+ * @since  2012/07/25
+ * @author GIP RECIA - Kevin Frapin <kevin.frapin@recia.fr>
+ */
+public interface ResultServiceFormService {
+
+    ///////////////////////////////////////////////////////
+    // WEEKLY RESULTS
+    ///////////////////////////////////////////////////////
+    /**
+     * Gets the result rows containing the data on the establishements, the services and the user profile.<br/>
+     * These data only concern the specified <code>week</code> of the specified <code>year</code>.<br/>
+     * Each result row is associated to one establishment, and contained two kinds of data :
+     * <ul>
+     * 	<li>The establishment data (name,...)</li>
+     * 	<li>The statistic data  (number of visits,...) indexed by service</li>
+     * </ul>
+     * In fact, in each result row, there is one statistic data per service.<br/>
+     * For more informations on the result row content, see {@link ResultRow}.
+     * 
+     * @param establishmentsUai
+     * 			The UAI of the establishements.
+     * @param services
+     * 			The services.
+     * @param userProfile
+     * 			The user profile.
+     * @param week
+     * 			The week number.
+     * @param year
+     * 			The year.
+     * 
+     * @return
+     * 	the result rows containing establishment data, and statistics data, for each user profile in each establishment.
+     */
+    public List<ResultRow> getPunctualWeekResultRows(List<String> establishmentsUai, List<String> services, String userProfile, Integer week, Integer year);
+    
+    
+    ///////////////////////////////////////////////////////
+    // MONTHLY RESULTS
+    ///////////////////////////////////////////////////////
+    /**
+     * Gets the result rows containing the data on the establishements and the users profiles.<br/>
+     * These data only concern the specified <code>month</code> of the specified <code>year</code>.<br/>
+     * Each result row is associated to one establishment, and contained two kinds of data :
+     * <ul>
+     * 	<li>The establishment data (name,...)</li>
+     * 	<li>The statistic data  (number of active accounts,...) indexed by user profile</li>
+     * </ul>
+     * In fact, in each result row, there is one statistic data per user profile.<br/>
+     * For more informations on the result row content, see {@link ResultRow}.
+     * 
+     * @param establishmentsUai
+     * 			The UAI of the establishements.
+     * @param services
+     * 			The services.
+     * @param userProfile
+     * 			The user profile.
+     * @param month
+     * 			The month number.
+     * @param year
+     * 			The year.
+     * 
+     * @return
+     * 	the result rows containing establishment data, and statistics data, for each user profile in each establishment.
+     */
+//    public List<ResultRow> getPunctualMonthResultRows(List<String> establishmentsUai, List<String> services, String userProfile, Integer month, Integer year);
+    
+    
+}
---
+++ indicators-domain-services/src/main/java/org/esco/indicators/services/form/service/ResultServiceFormServiceImpl.java
@@ -0,0 +1,193 @@
+/**
+ * 
+ */
+package org.esco.indicators.services.form.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.esco.indicators.domain.beans.result.EstablishmentData;
+import org.esco.indicators.domain.beans.result.ResultRow;
+import org.esco.indicators.domain.beans.result.statistic.PunctualAccountStatistic;
+import org.esco.indicators.domain.beans.result.statistic.ServiceStatistic;
+import org.esco.indicators.domain.beans.structure.Establishment;
+import org.esco.indicators.services.constants.ServicesConstants;
+import org.esco.indicators.services.statistic.AccountStatisticService;
+import org.esco.indicators.services.statistic.ServiceConnectionStatisticService;
+import org.esco.indicators.services.structure.EstablishmentService;
+
+/**
+ * Implementation of the {@link ResultServiceFormService} interface.
+ * 
+ * @since  2012/07/25
+ * @author GIP RECIA - Kevin Frapin <kevin.frapin@recia.fr>
+ */
+public class ResultServiceFormServiceImpl implements ResultServiceFormService {
+    //---------------------------------------------------------------------------------- ATTRIBUTES
+    /** Logger of the class */
+    private static final Logger LOGGER = Logger.getLogger(ResultServiceFormServiceImpl.class);
+    
+    /** Service providing access to account activation statistics data */
+    private AccountStatisticService accountStatisticService;
+    
+    /** Service providing access to establishment data */
+    private EstablishmentService establishmentService;
+    
+    /** Service providing access to the services connections statistics */
+    private ServiceConnectionStatisticService serviceConnectionStatisticService;
+    
+    //-------------------------------------------------------------------------------- CONSTRUCTORS
+
+
+    //--------------------------------------------------------------------------- GETTERS / SETTERS
+    
+    /**
+     * Sets the account activation service.
+     * 
+     * @param accountStatisticService 
+     * 			The account activation service to set.
+     */
+    public void setAccountStatisticService(
+    	AccountStatisticService accountStatisticService) {
+        this.accountStatisticService = accountStatisticService;
+    }
+    
+    /**
+     * Sets the establishment service.
+     * @param establishmentService 
+     * 				The establishment service to set.
+     */
+    public void setEstablishmentService(EstablishmentService establishmentService) {
+        this.establishmentService = establishmentService;
+    }
+
+    /**
+     * Sets the services statistics service.
+     * @param serviceConnectionStatisticService 
+     * 				The services statistics service to set.
+     */
+    public void setServiceConnectionStatisticService(
+    	ServiceConnectionStatisticService serviceConnectionStatisticService) {
+        this.serviceConnectionStatisticService = serviceConnectionStatisticService;
+    }
+
+
+
+    //------------------------------------------------------------------------------ PUBLIC METHODS
+    
+    ///////////////////////////////////////////////////////
+    // WEEKLY RESULTS
+    ///////////////////////////////////////////////////////
+    
+    /* (non-Javadoc)
+     * @see org.esco.indicators.services.form.service.ResultServiceFormService#getPunctualWeekResultRows(java.util.List, java.util.List, java.lang.String, java.lang.Integer, java.lang.Integer)
+     */
+    @Override
+    public List<ResultRow> getPunctualWeekResultRows(List<String> establishmentsUai, List<String> services,
+            String userProfile, Integer week, Integer year) {
+	// Final result
+	List<ResultRow> rows = new ArrayList<ResultRow>();
+
+	// For each establishment :
+	//	Creation of the corresponding result row
+	//	Addition of the establishment data in the result row
+	//	Addition of the statistic data in the result row (for each service)
+	for (String uai : establishmentsUai) {
+	    ResultRow resultRow = new ResultRow();
+	    resultRow.setEstablishmentData(getEstablishmentData(uai));
+	    for (String service : services) {
+		PunctualAccountStatistic statistic = createPunctualWeekStatisticData(uai, service, userProfile, week, year);
+		resultRow.putStatisticData(service, statistic);
+	    }
+	    rows.add(resultRow);
+	}
+	
+	return rows;
+    }
+
+
+
+    
+    ///////////////////////////////////////////////////////
+    // MONTHLY RESULTS
+    ///////////////////////////////////////////////////////
+    
+
+    //----------------------------------------------------------------------------- PRIVATE METHODS
+    
+    /**
+     * Retrieves data and creates the statistic data for a specified :
+     * <ul>
+     * 	<li>establishment</li>
+     * 	<li>service</li>
+     * 	<li>user profile</li>
+     * 	<li>week</li>
+     * 	<li>year</li>
+     * </ul>
+     * 
+     * @param establishmentUai
+     * 			The UAI of the establishment.
+     * @param service
+     * 			The service.
+     * @param userProfile
+     * 			The user profile.
+     * @param week
+     * 			The number of the week in the year.
+     * @param year
+     * 			The year.
+     * 
+     * @return
+     * 	the statistic data.
+     */
+    private ServiceStatistic createPunctualWeekStatisticData(String establishmentUai, String service, String userProfile, Integer week, Integer year) {
+	// Retrieval of the total account number
+	Integer totalAccountNumber = accountStatisticService.findWeeklyTotalNumAccountsForProfile(establishmentUai, userProfile, week, year);
+	
+	// Retrieval of the number of activated accounts
+	Integer numActivatedAccounts = accountStatisticService.findWeeklyNumActivatedAccountsForProfile(establishmentUai, userProfile, week, year);
+	
+	// Retrieval of the service visitors statistics with a number of connections below / above a treshold
+	Integer treshold = ServicesConstants.NUM_CONNECTIONS_TRESHOLD;
+	Integer numVisitorsAboveTreshold = serviceConnectionStatisticService.findWeeklyNumVisitorsAboveTreshold(establishmentUai, service, userProfile, treshold, week, year);
+	
+	// TODO : Change to findWeeklyNumVisitorsBelowTreshold
+	Integer numVisitorsBelowTreshold = serviceConnectionStatisticService.findWeeklyNumVisitorsAboveTreshold(establishmentUai, service, userProfile, treshold, week, year);
+	
+	// Retrieval of the number of visits realized on the service
+	Integer numVisits = serviceConnectionStatisticService.findWeeklyNumVisits(establishmentUai, service, userProfile, week, year);
+	
+	// Creation of the statistic data
+	ServiceStatistic data = new ServiceStatistic(totalAccountNumber, numActivatedAccounts, numVisitorsBelowTreshold, numVisitorsAboveTreshold, numVisits);
+	
+	return data;
+    }
+    
+    /**
+     * Gets the establishment data for the specified UAI.
+     * 
+     * @param uai
+     * 			The UAI of the establishment.
+     * 
+     * @return
+     * 	the result data associated to the establishment.<br/>
+     * 	<code>null</code> if no establishment data has been retrieved.
+     */
+    private EstablishmentData getEstablishmentData(String uai) {
+	// Gets the establishment
+	Establishment establishment = establishmentService.findEstablishmentByUai(uai);
+	if(establishment == null) {
+	    return null;
+	}
+	
+	// Map establishment to establishment data
+	String countyNumber = establishment.getCountyNumber().toString();
+	String name = establishment.getName();
+	String establishmentType = establishment.getType();
+	EstablishmentData data = new EstablishmentData(countyNumber, name, establishmentType, uai);
+	
+	return data;
+    }
+    
+    //------------------------------------------------------------------------------ STATIC METHODS
+}
