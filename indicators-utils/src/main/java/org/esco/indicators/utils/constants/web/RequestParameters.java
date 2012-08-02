149e03e424bd91b478e9e992682886a1b2b3c889
kfrapin <kevin.frapin@gmail.com>
2012-08-02 11:48:43 +0200

    Creation of constants used to retrieve request parameters

---
+++ indicators-utils/src/main/java/org/esco/indicators/utils/constants/web/RequestParameters.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.esco.indicators.utils.constants.web;
+
+/**
+ * Constants used for passing parameters into requests.
+ * 
+ * @since  2012/08/01
+ * @author GIP RECIA - Kevin Frapin <kevin.frapin@recia.fr>
+ */
+public class RequestParameters {
+    
+    /**Parameter : establishment uai */
+    public static final String ESTABLISHMENT_UAI = "uai";
+    
+}
