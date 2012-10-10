20e6b91fed30eb35381a18a32c4c6482ad279e81
kfrapin <kevin.frapin@gmail.com>
2012-10-10 10:40:56 +0200

    Addition of a method for retrieving establishments by properties names / values

--- indicators-domain-services/src/main/java/org/esco/indicators/services/structure/EstablishmentService.java
+++ indicators-domain-services/src/main/java/org/esco/indicators/services/structure/EstablishmentService.java
@@ -3,10 +3,11 @@
  */
 package org.esco.indicators.services.structure;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.esco.indicators.domain.beans.structure.Establishment;
+import org.esco.indicators.utils.constants.structure.EstablishmentConstants;
 
 /**
  * Interface providing wantedServices for access establishments data.
@@ -17,17 +18,7 @@ import org.esco.indicators.domain.beans.structure.Establishment;
 public interface EstablishmentService {
 
     /**
-     * Retrieves the establishment, in database, associated to the specified <code>uai</code>.
-     * 
-     * @param uai
-     *            The UAI of the searched establishment.
-     * @return the establishement associated to the <code>uai</code> if it has been retrieved.<br/>
-     *         <code>null</code> if no establishment has been retrieved.
-     */
-    public Establishment findEstablishmentByUai(String uai);
-
-    /**
-     * Retrieves the establishments, in database, associated to the specified <code>countyNumber</code>.
+     * Retrieves the establishments associated to the specified <code>countyNumber</code>.
      * 
      * @param countyNumber
      *            The county number of the searched establishments.
@@ -35,9 +26,9 @@ public interface EstablishmentService {
      *         an empty <code>List</code> if no estbalishment has been retrieved.
      */
     public List<Establishment> findEstablishmentsByCountyNumber(String countyNumber);
-
+    
     /**
-     * Retrieves the establishements, in database, that are geographically localised in the counties
+     * Retrieves the establishements that are geographically localised in the counties
      * (associated to the <code>countyNumbers</code>) and that have the same type as one of the specified
      * <code>types</code>.
      * 
@@ -54,8 +45,52 @@ public interface EstablishmentService {
     public List<Establishment> findEstablishmentsByCountyNumbersAndTypes(List<String> countyNumbers,
 	    List<String> types);
 
+    
     /**
-     * Retrieves the establishments, in database, associated to the specified <code>type</code>.
+     * Retrieves the establishments by their properties names and values.<br/>
+     * An establishment is retrieved is at least one of the property name and property value is contained in the establishment.<br/>
+     * <br/>
+     * 
+     * <u>For instance :</u><br/>
+     * If the given properties values and names are :
+     * <ul>
+     * 	<li>Property name 1 = "type"</li>
+     * 	<ul>
+     * 		<li>Property value 1.1 = "University"</li>
+     * 		<li>Property value 1.2 = "College"</li>
+     * 	</ul>
+     * 	<li>Property name 2 = "countyNumber"</li>
+     * 	<ul>
+     * 		<li>Property value 2.1 = "69"</li>
+     * 		<li>Property value 2.2 = "435"</li>
+     * 	</ul>
+     * </ul>
+     * 
+     * The following estbalishment will be retrieved :
+     * <ul>
+     * 	<li>Establishment property 1 = "type"</li>
+     * 	<ul>
+     * 		<li>Establishment value 1.1 = "University"</li>
+     * 	</ul>
+     * 	<li>Establishement property 2 = "countyNumber"</li>
+     * 	<ul>
+     * 		<li>Establishment value 2.1 = "234"</li>
+     * 	</ul>
+     * </ul>
+     * 
+     * It will be retrieved because the "type" property is equal to "University" which corresponds to one of the given properties names and values.
+     * 
+     * @param propertiesNamesAndValues
+     * 			The map containing the properties names associated to their respective values.
+     * 
+     * @return
+     * 	the list of estbalishments having at least one property name and value corresponding to the given ones.<br/>
+     * 	An empty list if no establishment has been retrieved.
+     */
+    public List<Establishment> findEstablishmentsByPropertiesNamesAndValues(HashMap<String, List<String>> propertiesNamesAndValues);
+    
+    /**
+     * Retrieves the establishments associated to the specified <code>type</code>.
      * 
      * @param type
      *            The type of the searched establishments.
@@ -63,6 +98,16 @@ public interface EstablishmentService {
      *         an empty <code>List</code> if no estbalishment has been retrieved.
      */
     public List<Establishment> findEstablishmentsByType(String type);
+    
+    /**
+     * Retrieves the establishment associated to the specified <code>uai</code>.
+     * 
+     * @param uai
+     *            The UAI of the searched establishment.
+     * @return the establishement associated to the <code>uai</code> if it has been retrieved.<br/>
+     *         <code>null</code> if no establishment has been retrieved.
+     */
+    public Establishment findEstablishmentByUai(String uai);
     
     /**
      * Gets the UAI of the establishments located in the county and having one of the specified type.
@@ -77,5 +122,6 @@ public interface EstablishmentService {
      * 	an empty list if no establishments UAI has been retrieved.
      */
     public List<String> findEstablishmentsUaiByCounty(String countyNumber, List<String> establishmentsTypes);
+    
 
 }
--- indicators-domain-services/src/main/java/org/esco/indicators/services/structure/EstablishmentServiceImpl.java
+++ indicators-domain-services/src/main/java/org/esco/indicators/services/structure/EstablishmentServiceImpl.java
@@ -4,12 +4,20 @@
 package org.esco.indicators.services.structure;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.SetUtils;
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.log4j.Logger;
 import org.esco.indicators.dao.structure.EstablishmentDao;
 import org.esco.indicators.domain.beans.structure.Establishment;
 import org.esco.indicators.services.ldap.LdapEstablishmentService;
+import org.esco.indicators.utils.constants.structure.EstablishmentConstants;
 
 /**
  * Implementation of the {@link EstablishmentService} interface.
@@ -132,6 +140,62 @@ public class EstablishmentServiceImpl implements EstablishmentService {
 	return establishments;
     }
     
+    //------------------------------------------------------------------------------ STATIC METHODS
+    
+    /* (non-Javadoc)
+     * @see org.esco.indicators.services.structure.EstablishmentService#findEstablishmentsByPropertiesNamesAndValues(java.util.HashMap)
+     */
+    @Override
+    public List<Establishment> findEstablishmentsByPropertiesNamesAndValues(
+            HashMap<String, List<String>> propertiesNamesAndValues) {
+	 // Final result
+	Set<Establishment> establishments = new HashSet<Establishment>();
+	
+	// Retrieves the establishments by properties names and values
+	for (String propertyName : propertiesNamesAndValues.keySet()) {
+	    List<String> values = propertiesNamesAndValues.get(propertyName);
+	    establishments.addAll(findEstablishmentsByPropertyNameAndValues(propertyName, values));
+	}
+	
+	return (new ArrayList<Establishment>(establishments));
+    }
+
+    /**
+     * Retieves the establishments by property name and values.<br/>
+     * The behavior is similar to {@link EstablishmentServiceImpl#findEstablishmentsByPropertiesNamesAndValues(HashMap)}.
+     * 
+     * @param propertyName
+     * 			The name of the property used to retrieved the establishments.
+     * @param propertyValues
+     * 			The values of the property used to retrieved the establisments.
+     * 
+     * @return
+     * 	the establishments having at least one property value associated to the property name.<br/>
+     * 	An empty set if no establishments has been retrieved.
+     */
+    private Set<Establishment> findEstablishmentsByPropertyNameAndValues(
+	    String propertyName, List<String> propertyValues) {
+	// Final result
+	Set<Establishment> establishments = new HashSet<Establishment>();
+	
+	// Retrieves the establishments by property
+	List<Establishment> retrievedEstbalishments = new ArrayList<Establishment>();
+	if(propertyName.equalsIgnoreCase(EstablishmentConstants.ESTAB_PROPERTY_COUNTY_NUMBER)) {
+	    retrievedEstbalishments = establishmentDao.findEstablishmentsByCountyNumbers(propertyValues);
+	} else if (propertyName.equalsIgnoreCase(EstablishmentConstants.ESTAB_PROPERTY_TYPE)) {
+	    retrievedEstbalishments = establishmentDao.findEstablishmentsByTypes(propertyValues);
+	} else if (propertyName.equalsIgnoreCase(EstablishmentConstants.ESTAB_PROPERTY_UAI)) {
+	    retrievedEstbalishments = establishmentDao.findEstablishmentByUais(propertyValues);
+	} else {
+	    // If the property name is unknown
+	    LOGGER.warn("The establishment property [" + propertyName + "] is unknown and is cannot be used for retrieving establishments");
+	}
+
+	 // Put the establishments into the set for removing duplicated entries
+	establishments.addAll(retrievedEstbalishments);
+	return establishments;
+    }
+
     /* (non-Javadoc)
      * @see org.esco.indicators.services.structure.EstablishmentService#findEstablishmentsByType(java.lang.String)
      */
