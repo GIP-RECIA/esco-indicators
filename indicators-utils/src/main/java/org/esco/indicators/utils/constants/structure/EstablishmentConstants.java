e154282468133fad56643aa6127dcd6772555dfe
kfrapin <kevin.frapin@gmail.com>
2012-10-10 10:38:40 +0200

    Addtion of constants for the establishment properties

---
+++ indicators-utils/src/main/java/org/esco/indicators/utils/constants/structure/EstablishmentConstants.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package org.esco.indicators.utils.constants.structure;
+
+/**
+ * Constants concerning the establishments.
+ * 
+ * @since  2012/07/10
+ * @author GIP RECIA - Kevin Frapin <kevin.frapin@recia.fr>
+ */
+public class EstablishmentConstants {
+
+    /** Establishment property : the county number */
+    public static final String ESTAB_PROPERTY_COUNTY_NUMBER = "countyNumber";
+    
+    /** Establishment property : the name */
+    public static final String ESTAB_PROPERTY_NAME = "name";
+    
+    /** Establishment property : the SIREN */
+    public static final String ESTAB_PROPERTY_SIREN = "siren";
+    
+    /** Establishment property : the type */
+    public static final String ESTAB_PROPERTY_TYPE = "type";
+    
+    /** Establishment property : the UAI */
+    public static final String ESTAB_PROPERTY_UAI = "uai";
+    
+}
