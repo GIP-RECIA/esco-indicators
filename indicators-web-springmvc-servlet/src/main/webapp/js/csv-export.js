d90e15ab59746d1b3d03e3ab32b6b39084074584
kfrapin <kevin.frapin@gmail.com>
2012-10-17 12:16:04 +0200

    Creation a script for exporting result table into CSV file

---
+++ indicators-web-springmvc-servlet/src/main/webapp/js/csv-export.js
@@ -0,0 +1,95 @@
+/************************************************
+ *
+ * Author : Frapin Kevin <kevin.frapin@recia.fr>
+ * Date : 2012/17/0
+ *
+ * **********************************************
+ *
+ * Description :
+ * -------------
+ * File containing functions used to export 
+ * table data into csv file.
+ *
+ * **********************************************
+ */
+
+
+///////////////////////////////////////////////////////////
+// INITIALIZATION FUNCTIONS
+///////////////////////////////////////////////////////////
+$(document).ready(function() {
+    ///////////////////////////////////////////////////////
+    // Handle the submission CSV export form
+    ///////////////////////////////////////////////////////
+    $("#csvexport").submit(function() {
+       // Transforms the table into csv data
+       var csvData = $("#resultTable").table2CSV();
+       // Injects the csv data into the hidden field
+       $("#csvData").prop("value", csvData);
+       // Creates a file name regarding to the selected filters
+       var fileName = createFileName();
+       fileName = fileName + ".csv";
+       // Injects the file name into the hidden field
+       $("#csvFileName").prop("value", fileName);
+    });
+});
+
+///////////////////////////////////////////////////////////
+// FUNCTIONS
+///////////////////////////////////////////////////////////
+/*
+ * Functions that creates a file name regarding to the 
+ * selected filters.
+ */
+function createFileName() {
+    // Gets the content of all the selected filters
+    var fileName = "";
+    $("#filters").find("span").filter(".filterContent").each(function() {
+        // Clean the data
+        var cleanData = cleanString($(this).html());
+        // Add the filter content to the file name
+        fileName = fileName + cleanData + " - ";
+    });
+    // Remove the last two characters
+    fileName = fileName.substring(0,fileName.length - 2);
+    return fileName;
+}
+
+/*
+ * Function that cleans the input data.
+ */
+function cleanString(input) {
+    // Removes the HTML tag
+    var regex  = new RegExp(/\<[^\<]+\>/g);
+    var output = regexReplace(regex, input, "");
+    // Removes the \n character
+    var regex = new RegExp(/\n/g);
+    var output = regexReplace(regex, output, "");
+    // Removes the tab, spaces,... at the begginning
+    var regex = new RegExp(/^(\s)+/g);
+    var output = regexReplace(regex, output, "");
+    // Removes the tab, spaces,... at the end
+    var regex = new RegExp(/(\s)+$/g);
+    var output = regexReplace(regex, output, "");
+    // Removes the duplicate tab, spaces,... characters
+    var regex = new RegExp(/(\s)+/g);
+    var output = regexReplace(regex, output, " ");
+    return output;
+}
+
+/*
+ * Function that replaces the matching elements
+ * by the replacement string into the input string.
+ *
+ * Parameters :
+ *  - regex : The RegExp element to match
+ *  - input : The input string to analyze
+ *  - replacment : The replacement string for the matched elements
+ *
+ *  Return :
+ *      The input string where the matched elements has been replaced
+ *      by the replacement string.
+ */
+function regexReplace(regex, input, replacement) {
+	return input.replace(regex, replacement);
+}
