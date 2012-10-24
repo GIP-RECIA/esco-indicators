dd5cc4159bfb02f83eb4572886e263959406fbd3
kfrapin <kevin.frapin@gmail.com>
2012-10-24 16:48:57 +0200

    Population of the user allowed properties

--- indicators-web-springmvc-servlet/src/main/java/org/esco/indicators/web/springmvc/controller/basic/BasicController.java
+++ indicators-web-springmvc-servlet/src/main/java/org/esco/indicators/web/springmvc/controller/basic/BasicController.java
@@ -3,13 +3,17 @@
  */
 package org.esco.indicators.web.springmvc.controller.basic;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
 import org.esco.indicators.domain.beans.people.User;
+import org.esco.indicators.domain.beans.permission.GenericFilter;
 import org.esco.indicators.services.auth.Authenticator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
@@ -40,6 +44,43 @@ public class BasicController {
     //--------------------------------------------------------------------------- GETTERS / SETTERS
 
     //------------------------------------------------------------------------------ PUBLIC METHODS
+    /**
+     * Populate the allowedProperties field.<br/>
+     * 
+     * The allowed properties are extracted from the permission filter associated
+     * to the authenticated user.<br/>
+     * 
+     * They allowed properties are stored into a map containing :
+     * <ul>
+     * 	<li>Keys : Properties names</li>
+     * 	<li>Values : Properties values</li>
+     * </ul>
+     * 
+     * This is made in order to test user permissions in the JSP.<br/>
+     * 
+     * @param request
+     * 			The request made by the user.
+     * @return
+     * 	the allowed properties.<br/>
+     * 	an empty map if no properties has been extracted from the permission filter.
+     */
+    @ModelAttribute("allowedProperties")
+    public Map<String, Set<String>> populateAllowedProperties(HttpServletRequest request) {
+ 	// Gets the establishment view permission filter
+	GenericFilter filter = authenticator.getEstablishmentFilter();
+	if(filter == null) {
+	    return new HashMap<String, Set<String>>();
+	}
+	
+	// Gets the allowed properties map
+	Map<String, Set<String>> allowedProperties = filter.getPropertiesNamesAndValues();
+	if(allowedProperties == null) {
+	    return new HashMap<String, Set<String>>();
+	}
+	
+	return allowedProperties;
+    }
+ 	
     /**
      * Populate the user field.
      * 
