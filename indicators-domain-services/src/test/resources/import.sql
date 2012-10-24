6026701558e95fa41bd69195fce149215831dde1
kfrapin <kevin.frapin@gmail.com>
2012-10-24 14:18:11 +0200

    Modification of import.sql (tables names,...)

--- indicators-domain-services/src/test/resources/import.sql
+++ indicators-domain-services/src/test/resources/import.sql
@@ -6,26 +6,26 @@ INSERT INTO etablissement (uai, departement, typeetablissement) VALUES ('0376543
 
 
 -- ACTIVATION DE COMPTE
-INSERT INTO estactivee (uid, datedebutactivation, datefinactivation) VALUES ('F1234567X', '2011-03-19', '2012-06-04');
-INSERT INTO estactivee (uid, datedebutactivation, datefinactivation) VALUES ('F8912345R', '2012-02-01', NULL);
-INSERT INTO estactivee (uid, datedebutactivation, datefinactivation) VALUES ('F8654397U', '2012-01-19', '2012-02-20');
+INSERT INTO est_activee (uid, datedebutactivation, datefinactivation) VALUES ('F1234567X', '2011-03-19', '2012-06-04');
+INSERT INTO est_activee (uid, datedebutactivation, datefinactivation) VALUES ('F8912345R', '2012-02-01', NULL);
+INSERT INTO est_activee (uid, datedebutactivation, datefinactivation) VALUES ('F8654397U', '2012-01-19', '2012-02-20');
 
 -- NOMBRE DE VISITEURS
 INSERT INTO nombredevisiteurs (jour, typeetab, typestat, uai, nbvisites, nbvisiteurs) VALUES ('2012-05-28', 'CFA', 'Etablissement', '0453456A', '450', '60');
 
 -- PROFILS UTILISATEURS
-INSERT INTO acommeprofil (uai, uid, nomprofil, datedebut, datefinprofil) VALUES ('0453456A', 'F1234567X', 'Teacher', '2011-03-18', '2012-07-13');
-INSERT INTO acommeprofil (uai, uid, nomprofil, datedebut, datefinprofil) VALUES ('0453456A', 'F8912345R', 'Teacher', '2011-09-17', NULL);
-INSERT INTO acommeprofil (uai, uid, nomprofil, datedebut, datefinprofil) VALUES ('0453456A', 'F8654397U', 'Teacher', '2012-02-01', NULL);
+INSERT INTO acommeprofil (uai, uid, nomprofil, datedebutprofil, datefinprofil) VALUES ('0453456A', 'F1234567X', 'Teacher', '2011-03-18', '2012-07-13');
+INSERT INTO acommeprofil (uai, uid, nomprofil, datedebutprofil, datefinprofil) VALUES ('0453456A', 'F8912345R', 'Teacher', '2011-09-17', NULL);
+INSERT INTO acommeprofil (uai, uid, nomprofil, datedebutprofil, datefinprofil) VALUES ('0453456A', 'F8654397U', 'Teacher', '2012-02-01', NULL);
 
 
 -- CONNEXIONS PROFILS AU PORTAIL (HEBDOMADAIRE)
-INSERT INTO connexionprofilsemaine (premierjoursemaine, uai, nomprofil, nbconnexionsemaine, nbpersonnesemaine, moyenneconnexionsemaine) VALUES ('2012-05-28', '0453456A', 'Teacher', '3', '25', '16.43');
-INSERT INTO connexionprofilsemaine (premierjoursemaine, uai, nomprofil, nbconnexionsemaine, nbpersonnesemaine, moyenneconnexionsemaine) VALUES ('2012-05-28', '0453456A', 'Teacher', '4', '13', '36.07');
+INSERT INTO connexionprofilsemaine (semaine, uai, nomprofil, nbconnexion, nbpersonne, moyenneconnexion) VALUES ('2012-05-28', '0453456A', 'Teacher', '3', '25', '16.43');
+INSERT INTO connexionprofilsemaine (semaine, uai, nomprofil, nbconnexion, nbpersonne, moyenneconnexion) VALUES ('2012-05-28', '0453456A', 'Teacher', '4', '13', '36.07');
 
 -- CONNEXIONS PROFILS AU PORTAIL (MENSUEL)
-INSERT INTO connexionprofilmois (mois, uai, nomprofil, nbconnexionmois, nbpersonnemois, moyenneconnexionmois) VALUES ('2012-05-01', '0453456A', 'Teacher', '7', '115', '46.21');
-INSERT INTO connexionprofilmois (mois, uai, nomprofil, nbconnexionmois, nbpersonnemois, moyenneconnexionmois) VALUES ('2012-05-01', '0453456A', 'Teacher', '9', '13', '16.21');
+INSERT INTO connexionprofilmois (mois, uai, nomprofil, nbconnexion, nbpersonne, moyenneconnexion) VALUES ('2012-05-01', '0453456A', 'Teacher', '7', '115', '46.21');
+INSERT INTO connexionprofilmois (mois, uai, nomprofil, nbconnexion, nbpersonne, moyenneconnexion) VALUES ('2012-05-01', '0453456A', 'Teacher', '9', '13', '16.21');
 
 
 
--- indicators-web-springmvc-servlet/src/test/resources/import.sql
+++ indicators-web-springmvc-servlet/src/test/resources/import.sql
@@ -19,9 +19,9 @@ INSERT INTO etablissement (uai, departement, typeetablissement) VALUES ('0180823
 
 
 -- ACTIVATION DE COMPTE
-INSERT INTO estactivee (uid, datedebutactivation, datefinactivation) VALUES ('F1234567X', '2011-03-19', '2012-06-04');
-INSERT INTO estactivee (uid, datedebutactivation, datefinactivation) VALUES ('F8912345R', '2012-02-01', NULL);
-INSERT INTO estactivee (uid, datedebutactivation, datefinactivation) VALUES ('F8654397U', '2012-01-19', '2012-02-04');
+INSERT INTO est_activee (uid, datedebutactivation, datefinactivation) VALUES ('F1234567X', '2011-03-19', '2012-06-04');
+INSERT INTO est_activee (uid, datedebutactivation, datefinactivation) VALUES ('F8912345R', '2012-02-01', NULL);
+INSERT INTO est_activee (uid, datedebutactivation, datefinactivation) VALUES ('F8654397U', '2012-01-19', '2012-02-04');
 
 -- NOMBRE DE VISITEURS
 INSERT INTO nombredevisiteurs (jour, typeetab, typestat, uai, nbvisites, nbvisiteurs) VALUES ('2012-05-28', 'CFA', 'Etablissement', '0450822X', '450', '60');
@@ -33,12 +33,12 @@ INSERT INTO acommeprofil (uai, uid, nomprofil, datedebut, datefinprofil) VALUES 
 
 
 -- CONNEXIONS PROFILS AU PORTAIL (HEBDOMADAIRE)
-INSERT INTO connexionprofilsemaine (premierjoursemaine, uai, nomprofil, nbconnexionsemaine, nbpersonnesemaine, moyenneconnexionsemaine) VALUES ('2012-05-28', '0450822X', 'ENTAuxEnseignant', '3', '25', '16.43');
-INSERT INTO connexionprofilsemaine (premierjoursemaine, uai, nomprofil, nbconnexionsemaine, nbpersonnesemaine, moyenneconnexionsemaine) VALUES ('2012-05-28', '0450822X', 'ENTAuxEnseignant', '4', '13', '36.07');
+INSERT INTO connexionprofilsemaine (premierjoursemaine, uai, nomprofil, nbconnexion, nbpersonne, moyenneconnexionsemaine) VALUES ('2012-05-28', '0450822X', 'ENTAuxEnseignant', '3', '25', '16.43');
+INSERT INTO connexionprofilsemaine (premierjoursemaine, uai, nomprofil, nbconnexion, nbpersonne, moyenneconnexionsemaine) VALUES ('2012-05-28', '0450822X', 'ENTAuxEnseignant', '4', '13', '36.07');
 
 -- CONNEXIONS PROFILS AU PORTAIL (MENSUEL)
-INSERT INTO connexionprofilmois (mois, uai, nomprofil, nbconnexionmois, nbpersonnemois, moyenneconnexionmois) VALUES ('2012-05-01', '0450822X', 'ENTAuxEnseignant', '7', '115', '46.21');
-INSERT INTO connexionprofilmois (mois, uai, nomprofil, nbconnexionmois, nbpersonnemois, moyenneconnexionmois) VALUES ('2012-05-01', '0450822X', 'ENTAuxEnseignant', '9', '13', '16.21');
+INSERT INTO connexionprofilmois (mois, uai, nomprofil, nbconnexion, nbpersonne, moyenneconnexionmois) VALUES ('2012-05-01', '0450822X', 'ENTAuxEnseignant', '7', '115', '46.21');
+INSERT INTO connexionprofilmois (mois, uai, nomprofil, nbconnexion, nbpersonne, moyenneconnexionmois) VALUES ('2012-05-01', '0450822X', 'ENTAuxEnseignant', '9', '13', '16.21');
 
 
 
