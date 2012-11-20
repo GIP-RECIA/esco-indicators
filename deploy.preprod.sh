5c902f14b969011142ff601e932f702f1f968cd3
kfrapin <kevin.frapin@gmail.com>
2012-11-20 11:40:06 +0100

    No versionning of preprod file

--- deploy.preprod.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-mvn -Dmaven.test.skip=true clean install && \
-scp /home/formation03/workspace/indicators/indicators-web-springmvc-servlet/target/indicators.war grouper@tournesol:/opt/webapps/indicators
