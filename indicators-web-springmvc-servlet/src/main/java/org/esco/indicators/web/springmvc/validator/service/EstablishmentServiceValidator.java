1fe51b563e70a9ad35eff56c7a795ea459471e8b
kfrapin <kevin.frapin@gmail.com>
2013-05-31 09:22:36 +0200

    Bug correction : the validator has to check the wanted services

--- indicators-web-springmvc-servlet/src/main/java/org/esco/indicators/web/springmvc/validator/service/EstablishmentServiceValidator.java
+++ indicators-web-springmvc-servlet/src/main/java/org/esco/indicators/web/springmvc/validator/service/EstablishmentServiceValidator.java
@@ -3,12 +3,17 @@
  */
 package org.esco.indicators.web.springmvc.validator.service;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.esco.indicators.domain.beans.form.ServiceForm;
 import org.esco.indicators.utils.constants.web.FormValidationConstants;
 import org.esco.indicators.utils.constants.xml.DataFormConstants;
+import org.esco.indicators.utils.list.ListUtils;
 import org.esco.indicators.web.springmvc.validator.basic.BasicValidator;
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 
 /**
  * Validator of the {@link ServiceForm}.
@@ -57,6 +62,26 @@ public class EstablishmentServiceValidator extends BasicValidator {
 	Integer maxEstablishments = FormValidationConstants.MAX_SELECTED_LOCAL_ESTABLISHMENTS;
 	if(establishments != null && establishments.length > maxEstablishments) {
 	    errors.rejectValue(DataFormConstants.ESTABLISHMENTS, "error.form.establishments.maxValue", new Object [ ] { maxEstablishments }, "error.form.establishments.maxValue");
+	}
+	
+	/////////////////////////////////////////////////////////////
+	// Validation of the selected services
+	/////////////////////////////////////////////////////////////
+	ValidationUtils.rejectIfEmpty(errors, DataFormConstants.WANTED_SERVICES, "error.form.wantedServices.empty");
+	
+	String [] wantedServicesArray = form.getWantedServices();
+	if(wantedServicesArray != null) {
+	    	List<String> wantedServices = Arrays.asList(wantedServicesArray);
+        	for (String wantedService : wantedServices) {
+        	    List<String> slavesServices = DataFormConstants.MASTERS_SLAVES_SERVICES.get(wantedService);
+        	    // If the wanted service is a master service
+        	    if(slavesServices != null) {
+        		// If one of the slave service is in the wanted services
+        		if(ListUtils.haveIntersection(wantedServices, slavesServices)) {
+        		    errors.rejectValue(DataFormConstants.WANTED_SERVICES, "error.form.wantedServices.sumOnly", new Object [ ] { }, "error.form.wantedServices.sumOnly");
+        		}
+        	    }
+        	}
 	}
 	
     }
